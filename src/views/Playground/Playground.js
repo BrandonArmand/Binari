53b0351c9ac7c4d480adacd0eff7613aedfc69c2
Brandon Welsh <bwelsh8@gmail.com>
2019-12-24 10:56:56 -0800

    Cleaned code up, added Canvas refreshing, and improved Code generation

--- src/views/Playground/Playground.js
+++ src/views/Playground/Playground.js
@@ -5,9 +5,9 @@ import Button from "components/CustomButtons/Button.js";
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
 import AceEditor from "react-ace";
-import Sketch from 'react-p5'
 import styles from "assets/jss/material-kit-react/views/playground.js";
 import Node from './tree'
+import Canvas from './canvas'
 import "ace-builds/src-min-noconflict/ext-language_tools";
 import "ace-builds/src-noconflict/mode-javascript";
 import "ace-builds/src-noconflict/snippets/javascript";
@@ -18,72 +18,12 @@ const beautify = js
 const useStyles = makeStyles(styles);
 
 export default function Playground(props) {
+  const classes = useStyles();
   const [currentCode, setCode] = useState(Node);
   const displayCode = beautify(currentCode)
-
-  const classes = useStyles();
-  const elementScale = window.devicePixelRatio < 2 ? 2 : window.devicePixelRatio
-  const windowSize = elementScale * 500
-  const gval = eval
-
   let unsavedCode = displayCode
-  let treeNode = gval(`(${currentCode})`)
-
+  let treeNode = new Function(`${currentCode}; return Node`)()
   let tree = new treeNode(50)
-      tree.x = windowSize / 2
-      tree.y = (20 * elementScale)
-  let drawQ = [tree]
-  let layerQ = []
-  let layer = 1
-
-  function setup(p5, canvasParentRef) {
-    p5.textSize((12 * elementScale))
-    p5.createCanvas(windowSize, windowSize).parent(canvasParentRef)
-    p5.frameRate(200)
-    
-    for (var i = 0; i < 25; i++) {
-      tree.insert(Math.floor(Math.random() * 100));
-    }
-  }
-
-  function draw(p5){
-    drawQ.forEach((el,i)=>{
-        let distanceX = ((windowSize / 2.4) / p5.pow(2, layer)) + 12
-        let distanceY = (20 * elementScale) + (layer * 9)
-
-        p5.strokeWeight(elementScale + 1)
-        p5.stroke(0)
-
-        if(el.left){
-            el.left.x = el.x - distanceX
-            el.left.y = el.y + distanceY
-            p5.line(el.x, el.y, el.left.x, el.left.y);
-            layerQ.push(el.left)
-        }
-
-        if(el.right){
-            el.right.x = el.x + distanceX
-            el.right.y = el.y + distanceY
-            p5.line(el.x, el.y, el.right.x, el.right.y);
-            layerQ.push(el.right)
-        }
-
-        p5.textAlign(p5.CENTER);
-        p5.stroke(0)
-        p5.strokeWeight(1)
-        p5.fill(p5.map(el.value,100,0,0,255),220,250);
-        p5.ellipse(el.x, el.y, (17 * elementScale))
-        p5.noStroke()
-        p5.fill('black')
-        p5.text(el.value, el.x, el.y + (6 + elementScale));
-
-        if(!drawQ[i+1]){
-            layer++
-            drawQ = [...layerQ]
-            layerQ = []
-        }
-    })
-  }
 
   return (
     <>
@@ -106,9 +46,10 @@ export default function Playground(props) {
                   />
                 </GridItem>
                 <GridItem xs={12} sm={7} md={7}>
-                  <h1>Binary Trees</h1><button onClick={()=>setCode(unsavedCode)}></button>
+                  <Button onClick={()=>setCode(unsavedCode)}>Run</Button>
+                  <h1>Binary Trees</h1>
                   <h2>Introduction</h2>
-                  <Sketch setup={setup} draw={draw} />
+                  <Canvas tree={tree} size={25} />
                 </GridItem>
             </GridContainer>
             </div>
---
+++ src/views/Playground/canvas.js
@@ -0,0 +1,67 @@
+import React from "react";
+import Sketch from 'react-p5'
+
+export default function Canvas(props){
+    const elementScale = window.devicePixelRatio < 2 ? 2 : window.devicePixelRatio
+    const windowSize = elementScale * 500
+    const treeSize = props.size
+
+    let tree = props.tree
+        tree.x = windowSize / 2
+        tree.y = (20 * elementScale)
+    let drawQ = [tree]
+    let layerQ = []
+    let layer = 1
+
+    for (var i = 0; i < treeSize; i++) {
+        tree.insert(Math.floor(Math.random() * 100));
+    }
+
+    function setup(p5, canvasParentRef) {
+        p5.textSize((12 * elementScale))
+        p5.createCanvas(windowSize, windowSize).parent(canvasParentRef)
+        p5.frameRate(200)
+    }
+
+    function draw(p5){
+        drawQ.forEach((el,i)=>{
+            let distanceX = ((windowSize / 2.4) / p5.pow(2, layer)) + 12
+            let distanceY = (20 * elementScale) + (layer * 9)
+
+            layer == 1 && p5.background('white')
+            p5.strokeWeight(elementScale + 1)
+            p5.stroke(0)
+
+            if(el.left){
+                el.left.x = el.x - distanceX
+                el.left.y = el.y + distanceY
+                p5.line(el.x, el.y, el.left.x, el.left.y);
+                layerQ.push(el.left)
+            }
+
+            if(el.right){
+                el.right.x = el.x + distanceX
+                el.right.y = el.y + distanceY
+                p5.line(el.x, el.y, el.right.x, el.right.y);
+                layerQ.push(el.right)
+            }
+
+            p5.textAlign(p5.CENTER);
+            p5.stroke(0)
+            p5.strokeWeight(1)
+            p5.fill(p5.map(el.value,100,0,0,255),220,250);
+            p5.ellipse(el.x, el.y, (17 * elementScale))
+            p5.noStroke()
+            p5.fill('black')
+            p5.text(el.value, el.x, el.y + (6 + elementScale));
+
+            if(!drawQ[i+1]){
+                layer++
+                drawQ = [...layerQ]
+                layerQ = []
+            }
+        })
+    }
+
+    return <Sketch setup={setup} draw={draw} />
+}
