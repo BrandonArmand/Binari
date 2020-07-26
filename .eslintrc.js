811ff6b4dbef7e819545e95dfd96861b3bf1c491
Danny (vai) San <vai.quoc.san@gmail.com>
2020-07-25 17:31:15 -0700

    eslintrc.js file updated with settings to detect React version

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,11 @@ module.exports = {
         },
     },
     plugins: ["react"],
+    settings: {
+        "react": {
+            "version": "detect",
+        },
+    },
     extends: [
         "eslint:recommended",
         "plugin:react/recommended"
