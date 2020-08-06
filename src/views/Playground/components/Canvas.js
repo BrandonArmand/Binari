e287882755b3f05409ff75e91823577447a31051
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-08-06 19:13:30 +0200

    Added an optional color property to nodes to draw a custom color

--- src/views/Playground/components/Canvas.js
+++ src/views/Playground/components/Canvas.js
@@ -2,11 +2,13 @@ import React from "react";
 import Sketch from "react-p5";
 import PropTypes from "prop-types";
 
+/*A map containing all custom colors name-value pairs*/
+let customColors;
+
 const Canvas = props => {
     const elementScale =
     window.devicePixelRatio < 2 ? 2 : window.devicePixelRatio;
     const windowSize = elementScale * 500;
-
     /*Array of all nodes*/
     var nodeList;
     /*All nodes represented as a reference to the root node*/
@@ -39,12 +41,20 @@ const Canvas = props => {
         p5.createCanvas(windowSize, 1000).parent(canvasParentRef);
         p5.frameRate(100);
         setTree();
+        setColors(p5);
     }
 
     function setTree() {
         nodeList = [];
         //Generate all position and layer data
         return positionNode(tree, 1, 1);
+    }
+
+    /**Fills the customColors map**/
+    function setColors(p5) {
+        customColors = new Map([
+            ["red", p5.color(255, 0, 0)],
+        ]);
     }
 
     function draw(p5) {
@@ -58,11 +68,22 @@ const Canvas = props => {
             if (el.right != null) {
                 p5.line(el.x, el.y, el.right.x, el.right.y);
             }
+
+            p5.text(el.color, el.x, el.y - (6 + elementScale));
+
+            //Retrieve custom color
+            let color = customColors.get(el.color);
+
+            //No custom color, draw the default one
+            if (color === undefined) {
+                color = p5.color(p5.map(el.value, 100, 0, 0, 255), 220, 250);
+            }
+
             //Draw node
             p5.textAlign(p5.CENTER);
             p5.stroke(0);
             p5.strokeWeight(1);
-            p5.fill(p5.map(el.value, 100, 0, 0, 255), 220, 250);
+            p5.fill(color);
             p5.ellipse(el.x, el.y, 17 * elementScale);
             p5.noStroke();
             p5.fill("black");
