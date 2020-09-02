5e67d42be91ba018efb574c0df26b20ece112fb8
Ankitkumar Boghra <ankitboghra@gmail.com>
2020-09-02 22:18:14 +0530

    formatting bullets

--- src/views/Playground/chapters/one/lesson.js
+++ src/views/Playground/chapters/one/lesson.js
@@ -1,8 +1,8 @@
 export default `
 # Insertion
 ## Fundamentals
-We begin from the root node.
-- Compare the new value with the value of the root node. If the new value is lesser than that of the root, we move to the left subtree.
+- We begin from the root node. Compare the new value with the value of the root node. 
+- If the new value is lesser than that of the root, we move to the left subtree.
 - Similarly, if the new value is greater than that of the root, we move to the right subtree.
 - This process is repeated until the we reach to a leaf node(i.e. node with no child).
 - Based on the leaf node, insert the new value to left or right.
