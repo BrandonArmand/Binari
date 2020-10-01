f5ab59c89f435415dccfe80aeef3d651e37f06cf
thexdev <akbarnugroho253@gmail.com>
2020-10-01 22:39:36 +0700

    feat: change the gray color
    Enhance the web accessibility by increasing the background and foreground colors contrast ratio

--- src/assets/jss/material-kit-react.js
+++ src/assets/jss/material-kit-react.js
@@ -75,7 +75,7 @@ const dangerColor = "#f44336";
 const successColor = "#4caf50";
 const infoColor = "#00acc1";
 const roseColor = "#e91e63";
-const grayColor = "#999999";
+const grayColor = "#333";
 
 const primaryBoxShadow = {
     boxShadow:
