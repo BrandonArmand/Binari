bf0e6b0c07f10194762d25f5a3ad20679a08b1e6
Brandon Welsh <contact@brandonarmand.com>
2022-12-12 21:48:12 -0800

    revert about file from commit 62e2958

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import classNames from "classnames";
 import { makeStyles } from "@material-ui/core/styles";
 import GridContainer from "components/Grid/GridContainer.js";
@@ -8,43 +8,57 @@ import styles from "assets/jss/material-kit-react/views/aboutPage.js";
 import Card from "./components/Card";
 
 import { commitAdd, commitDel } from "./helpers";
-import useGetContributors from "./components/useGetContributors";
 
 const useStyles = makeStyles(styles);
 
+const apiHeaders = new Headers();
+apiHeaders.append("Authorization", `Bearer ${process.env.REACT_APP_TOKEN}`);
+const reqOptions = {
+    method: "GET",
+    headers: apiHeaders,
+    redirect: "follow",
+};
+
 export default function About() {
     const classes = useStyles();
-    const url =
-        "https://api.github.com/repos/brandonarmand/binari/stats/contributors";
-    const { contributors } = useGetContributors(url);
+    const [ contributors, setContributors ] = useState(null);
+
+    useEffect(() => {
+        fetch(
+            "https://api.github.com/repos/brandonarmand/binari/stats/contributors",
+            reqOptions
+        )
+            .then(response => response.json())
+            .then(data =>
+                !data.message &&
+                setContributors(data.sort((a, b) => (a.total < b.total ? 1 : -1)))
+            )
+            .catch(error => void error);
+    }, []);
+
     return (
         <div>
             <div
                 className={
-                    "floating-main " +
-                    classNames(classes.main, classes.mainRaised)
+                    "floating-main " + classNames(classes.main, classes.mainRaised)
                 }
             >
                 <div className={classes.container}>
                     <GridContainer justify="flex-start">
                         <GridItem xs={12} sm={12} md={12}>
-                            <h1
-                                style={{ textAlign: "center" }}
-                                className={classes.title}
-                            >
+                            <h1 style={{ textAlign: "center", }} className={classes.title}>
                                 About Binari
                             </h1>
                         </GridItem>
                         <GridItem xs={12} sm={12} md={12}>
                             <h1 className={classes.title}>Mission Statement</h1>
                             <h2 className={classes.subtitle}>
-                                Binari&apos;s mission is to provide the best
-                                experience for new developers through an easy to
-                                use code-editor and data-structure visualizer.
-                                The problem with learning a new algorithm or
-                                data-structure tends to lie in understanding how
-                                the data/code gets organized. I am here to fix
-                                that, and you are welcome to join.
+                                Binari&apos;s mission is to provide the best experience for new
+                                developers through an easy to use code-editor and data-structure
+                                visualizer. The problem with learning a new algorithm or
+                                data-structure tends to lie in understanding how the data/code
+                                gets organized. I am here to fix that, and you are welcome to
+                                join.
                             </h2>
                         </GridItem>
                     </GridContainer>
@@ -70,7 +84,7 @@ export default function About() {
 
                         <h1 className={classes.title}>Contributors</h1>
                         {contributors != null ? (
-                            contributors.map((el) => {
+                            contributors.map(el => {
                                 return (
                                     <Card
                                         key={el.author.html_url}
@@ -100,15 +114,12 @@ export default function About() {
                         <GridItem xs={12} sm={12} md={12}>
                             <h2 className={classes.title}>Want to help?</h2>
                             <h2 className={classes.subtitle}>
-                                There are a list of issues on Github that you
-                                are welcome to work on. If you have an idea of
-                                your own, we are open to allowing any pull
-                                requests we believe fit well with Binari&apos;s
-                                vision.
+                                There are a list of issues on Github that you are welcome to
+                                work on. If you have an idea of your own, we are open to
+                                allowing any pull requests we believe fit well with
+                                Binari&apos;s vision.
                             </h2>
-                            <h3 className={classes.listTitle}>
-                                Things you can do:
-                            </h3>
+                            <h3 className={classes.listTitle}>Things you can do:</h3>
                         </GridItem>
                         <GridItem xs={6} sm={4} md={4}>
                             <ul className={classes.subtitle}>
@@ -132,9 +143,7 @@ export default function About() {
                         </GridItem>
                         <GridItem xs={12} sm={12} md={12}>
                             <a href="https://github.com/BrandonArmand/Binari/issues">
-                                <h4 className={classes.footer}>
-                                    Start Contributing
-                                </h4>
+                                <h4 className={classes.footer}>Start Contributing</h4>
                             </a>
                         </GridItem>
                     </GridContainer>
